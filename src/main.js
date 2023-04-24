ac85304f72d73594ac497237b5af730447d9addd
jinchenxing <2541226493@qq.com>
2023-04-24 13:10:22 +0800

    feat(custom): 全局引入图标

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
 
@@ -13,6 +14,9 @@ app.use(createPinia())
 app.use(router)
 app.use(ElementPlus)
 
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 import 'virtual:windi.css'
 
 app.mount('#app')
