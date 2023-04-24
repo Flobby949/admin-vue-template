7010706bf51c04a9bf9aef4cc40e9be6dbc3ce4f
jinchenxing <2541226493@qq.com>
2023-04-24 13:17:00 +0800

    feat(custom): 配置自动导入

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,21 +1,17 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   // 表示当前目录即为根目录，ESLint 规则将被限制到该目录下
   root: true,
   env: {
     // 在 node 环境下启动 ESLint 检测
-    node: true,
+    node: true
   },
   // ESLint 中基础配置需要继承的配置：vue3 的必要和标准配置
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-prettier",
-  ],
+  extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest'
   },
   /**
    * 需要修改的启用规则及其各自的错误级别
@@ -25,8 +21,9 @@ module.exports = {
    * "error" 或 2 - 开启规则，使用错误级别的错误：error (当被触发的时候，程序会退出)
    */
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "vue/multi-word-component-names": "off" // 组件名不需要多个单词
-  },
-};
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off', // 组件名不需要多个单词
+    'no-undef': 'off' // 忽略未定义的报错
+  }
+}
