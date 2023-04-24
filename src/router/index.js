2b639a1eafbff98fe74e5a85cdfaa8b6d53b36cc
jinchenxing <2541226493@qq.com>
2023-04-24 13:13:47 +0800

    feat(custom): 配置路由和404

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,30 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
+import Index from '@/views/index.vue'
+import Login from '@/views/login/index.vue'
+import NotFound from '@/views/404.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'index',
+    component: Index
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: NotFound
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes,
+  history: createWebHistory()
 })
 
 export default router
