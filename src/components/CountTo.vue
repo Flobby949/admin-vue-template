9ab61613c2dc443f3dc5bea6862252988f5d83ec
jinchenxing <2541226493@qq.com>
2023-04-25 14:45:03 +0800

    feat(custom): 统计面板数字动画组件

---
+++ src/components/CountTo.vue
@@ -0,0 +1,33 @@
+<template>
+  <!-- 不保留小数 -->
+  {{ d.num.toFixed(0) }}
+</template>
+
+<script setup>
+import gsap from 'gsap'
+// 定义该数字动画组件的属性 value，指定类型和默认值
+const props = defineProps({
+  value: {
+    type: Number,
+    default: 0
+  }
+})
+
+const d = reactive({ num: 0 })
+
+// 定义数字动画函数，1秒内从 0 变化到父组件通过 props 传入的值
+const animateToValue = () => {
+  gsap.to(d, {
+    duration: 1,
+    num: props.value
+  })
+}
+
+animateToValue()
+
+// 监听值的变化
+watch(
+  () => props.value,
+  () => animateToValue()
+)
+</script>
